b7d4fb9b5bc912a21ec354e1163223227c07307d
Arijit166 <arijit.sarkar7156@gmail.com>
2025-12-23 00:18:21 +0530

    Fixed for deployment

--- app/api/auth/google/route.ts
+++ app/api/auth/google/route.ts
@@ -2,16 +2,16 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
-  const isSignup = searchParams.get('signup'); // Check if it's from signup page
-  
+  const isSignup = searchParams.get('signup'); 
+  const baseUrl = process.env.NEXTAUTH_URL
   const params = new URLSearchParams({
     client_id: process.env.GOOGLE_CLIENT_ID as string,
-    redirect_uri: 'http://localhost:3000/api/google-callback',
+    redirect_uri: `${baseUrl}/api/google-callback`,
     scope: 'openid profile email',
     response_type: 'code',
     access_type: 'offline',
     prompt: 'select_account',
-    ...(isSignup && { state: 'signup' }) // Pass signup flag as state
+    ...(isSignup && { state: 'signup' })
   });
   
   const googleAuthUrl = `https://accounts.google.com/o/oauth2/auth?${params}`;
